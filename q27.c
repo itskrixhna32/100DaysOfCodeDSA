1fe434228d318c03875d22b6b4132f3272268d82
itskrixhna32 <itskrixhna32@gmail.com>
2026-02-27 17:53:00 +0000

    Doubly Linked List Insertion and Traversal - Implement using linked list with dynamic memory allocation.

---
+++ q27.c
@@ -0,0 +1,89 @@
+/*Problem: Doubly Linked List Insertion and Traversal - Implement using linked list with dynamic memory allocation.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for doubly linked list node
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at end of doubly linked list
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+
+    // If list is empty
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+
+    // Traverse to last node
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
+// Traverse forward
+void traverseForward(struct Node* head) {
+    struct Node* temp = head;
+    printf("Forward Traversal: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+// Traverse backward
+void traverseBackward(struct Node* head) {
+    if (head == NULL) return;
+
+    struct Node* temp = head;
+
+    // Go to last node
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    printf("Backward Traversal: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->prev;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, value;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter value: ");
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    traverseForward(head);
+    traverseBackward(head);
+
+    return 0;
+}
