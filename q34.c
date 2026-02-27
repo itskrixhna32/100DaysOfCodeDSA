9dddfa68f3589e73de9bd318119a4fdffe2b0d1a
itskrixhna32 <itskrixhna32@gmail.com>
2026-02-27 10:00:00 +0530

    Implement two stacks in a single array.

---
+++ q34.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+// Push
+void push(char x) {
+    stack[++top] = x;
+}
+
+// Pop
+char pop() {
+    if (top == -1)
+        return -1;
+    return stack[top--];
+}
+
+// Peek
+char peek() {
+    return stack[top];
+}
+
+// Precedence function
+int precedence(char x) {
+    if (x == '^') return 3;
+    if (x == '*' || x == '/') return 2;
+    if (x == '+' || x == '-') return 1;
+    return 0;
+}
+
+// Infix to Postfix
+void infixToPostfix(char infix[], char postfix[]) {
+    int i = 0, k = 0;
+    char ch;
+
+    while ((ch = infix[i++]) != '\0') {
+
+        // Operand
+        if (isalnum(ch)) {
+            postfix[k++] = ch;
+        }
+        // Left parenthesis
+        else if (ch == '(') {
+            push(ch);
+        }
+        // Right parenthesis
+        else if (ch == ')') {
+            while (top != -1 && peek() != '(') {
+                postfix[k++] = pop();
+            }
+            pop(); // remove '('
+        }
+        // Operator
+        else {
+            while (top != -1 && precedence(peek()) >= precedence(ch)) {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    // Pop remaining operators
+    while (top != -1) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+}
+
+// Main function
+int main() {
+    char infix[MAX], postfix[MAX];
+
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
