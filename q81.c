06d5aad5036b0fe217b2a1d19b40cdc72d827c50
itskrixhna32 <itskrixhna32@gmail.com>
2026-04-15 10:00:00 +0530

    Add iterative binary search implementation

---
+++ q81.c
@@ -0,0 +1,55 @@
+// Day 81 Question 1: Implement Bubble Sort
+// Sort an array using Bubble Sort algorithm
+
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int swapped = 0;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = 1;
+            }
+        }
+        // If no swapping happened, array is sorted
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Original array: ");
+    printArray(arr, n);
+    
+    bubbleSort(arr, n);
+    
+    printf("Sorted array: ");
+    printArray(arr, n);
+    
+    return 0;
+}
