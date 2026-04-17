cdf820a62aaac9aaaae518c10ec9f513d5cc9304
itskrixhna32 <itskrixhna32@gmail.com>
2026-04-17 10:00:00 +0530

    Add book allocation problem solution

---
+++ q83.c
@@ -0,0 +1,53 @@
+// Day 83 Question 1: Implement Selection Sort
+// Sort an array using Selection Sort algorithm
+
+#include <stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        if (minIdx != i) {
+            swap(&arr[i], &arr[minIdx]);
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Original array: ");
+    printArray(arr, n);
+    
+    selectionSort(arr, n);
+    
+    printf("Sorted array: ");
+    printArray(arr, n);
+    
+    return 0;
+}
