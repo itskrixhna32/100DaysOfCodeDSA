29ef3996caa2cf11dfdacbfcb224308cf2241fb4
itskrixhna32 <itskrixhna32@gmail.com>
2026-04-30 10:00:00 +0530

    Add topological sort using Kahn's BFS algorithm

---
+++ q96.c
@@ -0,0 +1,71 @@
+// Day 96 Question 1: Counting Inversions in Array
+// Count number of inversions in an array using merge sort
+
+#include <stdio.h>
+#include <stdlib.h>
+
+long long mergeAndCount(int arr[], int temp[], int left, int mid, int right) {
+    int i = left, j = mid + 1, k = left;
+    long long invCount = 0;
+    
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+            invCount += (mid - i + 1);
+        }
+    }
+    
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+    
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+    
+    for (i = left; i <= right; i++) {
+        arr[i] = temp[i];
+    }
+    
+    return invCount;
+}
+
+long long mergeSortAndCount(int arr[], int temp[], int left, int right) {
+    long long invCount = 0;
+    
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        
+        invCount += mergeSortAndCount(arr, temp, left, mid);
+        invCount += mergeSortAndCount(arr, temp, mid + 1, right);
+        invCount += mergeAndCount(arr, temp, left, mid, right);
+    }
+    
+    return invCount;
+}
+
+long long countInversions(int arr[], int n) {
+    int* temp = (int*)malloc(n * sizeof(int));
+    long long result = mergeSortAndCount(arr, temp, 0, n - 1);
+    free(temp);
+    return result;
+}
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    long long inversions = countInversions(arr, n);
+    printf("Number of inversions: %lld\n", inversions);
+    
+    return 0;
+}
